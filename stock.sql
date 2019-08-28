b3fd5fa809313db51ebdc10b13490097b5f35329
hechao <root@iot2.electrodragon.com>
2019-08-28 16:14:05 -0400

    update

--- stock.sql
+++ stock.sql
@@ -29,7 +29,7 @@ CREATE TABLE `stock` (
   `price` float(50,3) DEFAULT NULL,
   `share` int(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,8 +38,34 @@ CREATE TABLE `stock` (
 
 LOCK TABLES `stock` WRITE;
 /*!40000 ALTER TABLE `stock` DISABLE KEYS */;
-INSERT INTO `stock` VALUES (17,'DBC','2019-08-28 00:00:00',14.300,6);
+INSERT INTO `stock` VALUES (18,'AMLP','2019-08-26 00:00:00',8.860,1),(19,'COMT','2019-08-26 00:00:00',30.620,1),(20,'EWM','2019-08-23 00:00:00',27.645,1),(21,'DBC','2019-08-23 00:00:00',14.770,1),(22,'VNM','2019-08-23 00:00:00',15.700,1),(23,'EPOL','2019-08-22 00:00:00',20.620,1),(24,'GWX','2019-08-22 00:00:00',28.550,1),(25,'SRET','2019-07-09 00:00:00',14.910,1),(26,'IHY','2019-07-09 00:00:00',24.780,1),(28,'EWM','2019-08-28 15:10:24',29.315,1),(29,'VNM','2019-08-28 15:11:27',16.720,1),(30,'DBC','2019-08-28 00:00:00',14.300,5),(31,'BLV','2019-08-28 00:00:00',91.510,6);
 /*!40000 ALTER TABLE `stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sum1`
+--
+
+DROP TABLE IF EXISTS `sum1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sum1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `total_share` int(100) DEFAULT NULL,
+  `total_cost` float(50,3) DEFAULT NULL,
+  `avg_price` float(50,3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sum1`
+--
+
+LOCK TABLES `sum1` WRITE;
+/*!40000 ALTER TABLE `sum1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sum1` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-28 14:44:52
+-- Dump completed on 2019-08-28 16:13:48
